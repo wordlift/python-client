b1d76a0a1911d5c52d3ad8d0f83e6a30b70a5da3
David Riccitelli <david@wordlift.io>
2025-03-20 16:24:04 +0200

    remove Python 3.7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # http://pypi.python.org/pypi/setuptools
 NAME = "wordlift-client"
 VERSION = "1.76.0"
-PYTHON_REQUIRES = ">=3.7"
+PYTHON_REQUIRES = ">=3.8"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
     "python-dateutil",
