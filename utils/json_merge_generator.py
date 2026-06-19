9693c8f3f6b49d8332fcfa837d3dcbd3320de1d1
Rubens Panfili <rubens.panfili@gmail.com>
2026-06-19 14:39:06 +0200

    fix(build): sort spec files for deterministic merge ordering

--- utils/json_merge_generator.py
+++ utils/json_merge_generator.py
@@ -10,11 +10,11 @@ output_json_file = "./openapi-merge.json"
 # Gather all YAML files in the directory. The openapi merge operation takes the first security scheme and api server
 # that if finds, so that we provide them using the base.yaml file, see https://github.com/robertmassaioli/openapi-merge/tree/main/packages/openapi-merge#merging-behaviour
 # We do this because it happened that bogus yaml files broke the Python Client.
-yaml_files = [os.path.join(yaml_directory, "base", "base.yaml")] + [
+yaml_files = [os.path.join(yaml_directory, "base", "base.yaml")] + sorted([
     os.path.join(yaml_directory, f)
     for f in os.listdir(yaml_directory)
     if f.endswith(".yaml")
-]
+])
 
 # Construct the JSON structure
 json_data = {
